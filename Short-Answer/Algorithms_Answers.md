8ab1413a9f722891acff7945f3b90e391c9099db
“kulkarohan” <kulkarohan@users.noreply.github.com>
2020-02-21 14:56:36 -0500

    short answers and recurisve_count_th

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,21 @@
 
 ## Exercise I
 
-a)
+a) O(1) because no matter what the input size of n, the number of operations remains constant. 
 
 
-b)
+b) O(log n) because even though the range is declared as n number of elements, the loop only iterates j times in 2^j until that total surpasses n.
 
 
-c)
+c) O(n) because the function bunnyEars is recursively being called n times before reaching its base case.
 
 ## Exercise II
+
+1) Find the middle floor of the n-floor building and drop an egg
+    a) If the egg breaks, eliminate all floors below
+    b) If the egg doesn't break, eliminate all floors above
+2) Repeat on sub-sections of floors based on egg-drop results
+3) Stop when you've reached the lowest floor that the egg doesn't break from
+4) Runtime complexity: O(log n)
 
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,25 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-def count_th(word):
-    
-    # TBC
-    
-    pass
+def count_th(word, occurences=None):
+    # Base Case
+    if len(word) <= 1:
+        return 0
+
+    # Initialize data structure to keep count of "th" occurences
+    if occurences is None:
+        occurences = []
+
+    # Logic to move towards base case
+    if word[0] == 't' and word[1] == 'h':
+        occurences.append(1)
+        count_th(word[2:], occurences)
+
+    # If first char isn't a 't' then remove from word 
+    else:
+        count_th(word[1:], occurences)
+   
+    return len(occurences)
+
+if __name__ == "__main__":
+    print(count_th('abcthxththtyz'))
